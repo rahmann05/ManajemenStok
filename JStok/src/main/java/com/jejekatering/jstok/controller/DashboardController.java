5dc8302a3effd1c63957c7485ecc563f4f12863f
rahman <safdar23@if.unjani.ac.id>
2025-12-20 16:12:48 +0700

    fix: Menambahkan navigasi untuk halaman bahan di dashboard

--- JStok/src/main/java/com/jejekatering/jstok/controller/DashboardController.java
+++ JStok/src/main/java/com/jejekatering/jstok/controller/DashboardController.java
@@ -1,13 +1,16 @@
 package com.jejekatering.jstok.controller;
 
+package com.jejekatering.jstok.controller;
+
 import com.jejekatering.jstok.util.SessionManager;
 import com.jejekatering.jstok.model.Pengguna;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 import java.io.IOException;
 
 public class DashboardController {
@@ -19,15 +22,29 @@ public class DashboardController {
     private Label roleLabel;
 
     @FXML
-    public void initialize() {
-        // Ambil user dari SessionManager saat dashboard dibuka
-        Pengguna user = SessionManager.getCurrentUser();
+    private BorderPane mainBorderPane;
 
+    @FXML
+    public void initialize() {
+        Pengguna user = SessionManager.getCurrentUser();
         if (user != null) {
             welcomeLabel.setText("Halo, " + user.getUsername());
             roleLabel.setText(user.getRole());
         } else {
             welcomeLabel.setText("Halo, Tamu");
+        }
+    }
+    @FXML
+    protected void onMenuBahanClick() {
+        loadPage("BahanView");
+    }
+
+    private void loadPage(String pageName) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/jejekatering/jstok/view/" + pageName + ".fxml"));
+            mainBorderPane.setCenter(root); // Ganti bagian tengah saja
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
