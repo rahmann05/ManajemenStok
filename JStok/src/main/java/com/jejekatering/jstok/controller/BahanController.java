c1a2b7fe61247f8e32ab710d60e3e29f289f2552
duykasep <yudakurniawan23@if.unjani.ac.id>
2025-12-21 14:18:28 +0700

    Fix: Update css, memperbaiki tampilan setiap halaman dari login sampai laporan

--- JStok/src/main/java/com/jejekatering/jstok/controller/BahanController.java
+++ JStok/src/main/java/com/jejekatering/jstok/controller/BahanController.java
@@ -1,5 +1,6 @@
 package com.jejekatering.jstok.controller;
 
+import animatefx.animation.FadeInUp;
 import com.jejekatering.jstok.dao.BahanDAO;
 import com.jejekatering.jstok.model.Bahan;
 import javafx.collections.ObservableList;
@@ -7,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Duration;
 
 public class BahanController {
 
@@ -30,6 +32,7 @@ public class BahanController {
     public void initialize() {
         bahanDAO = new BahanDAO();
         loadData();
+        playEntryAnimations();
     }
 
     private void loadData() {
@@ -41,5 +44,11 @@ public class BahanController {
 
         ObservableList<Bahan> listData = bahanDAO.getAllBahan();
         bahanTable.setItems(listData);
+    }
+
+    private void playEntryAnimations() {
+        if (bahanTable != null) {
+            new FadeInUp(bahanTable).setDelay(Duration.millis(100)).play();
+        }
     }
 }
